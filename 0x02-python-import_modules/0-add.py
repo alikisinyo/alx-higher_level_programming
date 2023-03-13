54faf1e1d8fb4e08e3233873126dc090f71b1600
alikisinyo <hansadie2009@gmail.com>
2023-03-13 05:07:35 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,7 @@
-#!/usr/python3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 
-def add(a, b):
-    return(a + b)
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
